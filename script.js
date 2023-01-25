2af3964bc9b94ec218e16c5f4d81d48d3c1af7b4
BiancaDias <bianca.dias@ufv.br>
2023-01-25 19:54:30 -0300

    teste

---
+++ script.js
@@ -0,0 +1,44 @@
+exibirQuiz();
+function erro(exibiErro){
+    console.log("ERRO FOI DO TIPO " + exibiErro.response.status);
+}
+function exibirQuiz(){
+    //fazendo com o id 1 por exemplo
+    const quiz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1')
+    quiz.then(exibirQuizFuncionou);
+    quiz.catch(erro);
+}
+
+function exibirQuizFuncionou(Carregarquiz){
+    quizExibido = Carregarquiz.data;
+    console.log(quizExibido);
+    const layout = document.querySelector('.segunda-tela');
+    layout.innerHTML = "";
+    let template = "";
+    layout.innerHTML =`<div class="segunda-tela">
+    <div class="banner">
+        <h2>${quizExibido.title}</h2>
+        <img src = "${quizExibido.image}" class="portrait">
+        <div class="filtro"></div>
+    </div>
+    <div class="quiz">`;
+
+    for(let i=0; i<quizExibido.questions.length; i++){
+        template = `<div class="pergunta">
+        <div class="titulo-pergunta">
+            <h3>${quizExibido.questions[i].title}</h3>
+        </div>
+        <div class="alternativas">`
+        for(let j=0; j< quizExibido.questions[i].answers.length; j++){
+            `<div class="resposta">
+                <img src = "${quizExibido.questions[i].answers[j].image}">
+                <p>${quizExibido.questions[i].answers[j].text}</p>
+            </div>`
+        }
+        `</div>`;
+        layout.innerHTML = layout.innerHTML + template;
+    }
+
+    layout.innerHTML = layout.innerHTML + `</div>`;
+
+}
